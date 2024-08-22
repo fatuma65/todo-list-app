2ef7cc12d508fff0499051ec2a21e335d7132142
fatuma65 <namalefatuma28@gmail.com>
2024-08-22 16:54:10 +0300

    implement create tasks functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import HomePage from "./components/HomePage";
 import { Route, Routes } from "react-router-dom";
-import Tasks from "./components/Tasks";
-import CreateTasks from "./components/CreateTasks";
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/tasks" element={ <Tasks />} />
-      <Route path="/add/tasks" element={ <CreateTasks />} />
     </Routes>
-      
-
     </>
   );
 }
